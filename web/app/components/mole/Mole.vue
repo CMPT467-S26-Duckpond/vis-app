e3b60b8ff66c9afc2d277afcc2106638b011f7da
Miro-R <miro.rogers@gmail.com>
2026-04-03 15:06:36 -0700

    got a green circle rendering

--- web/app/components/mole/Mole.vue
+++ web/app/components/mole/Mole.vue
@@ -3,10 +3,7 @@
 
 
 <template>
-    <svg v-bind:id="moleId">
-        <circle rx={{xAxis}} ry={{yAxis}}>
-
-        </circle>
+    <svg v-bind:id="moleId" ref="svg">
     </svg>
 </template>
 
@@ -16,45 +13,48 @@
 //                                                              VS
 // const {a,b,c} = defineProps({a:type, b:type, c:type});
 
+
 const {xAxis, yAxis, moleId} = defineProps<{ 
     xAxis: number, 
     yAxis: number, 
     moleId: string }>();
 
-    const svg = ref(null)
-
-
+    
     import {
         fetchCsv,
         getLatestYearWaterUsage,
         WATER_VARIABLES,
         type ParsedCsv,
     } from "./dataUtils";
-
+    
     // import TimeSlider from "./timeSlider/TimeSlider.vue"; // STEAL BEA'S SLIDER
-
+    
     import * as d3 from "d3";
     import { ref, onMounted, useTemplateRef } from "vue";
-
-    const drawMole = () => {
-        
-        console.log("drawing Mole");
-        const mole = d3.select(moleId); // How to reference a prop in script vue
-        mole.append('circle')
-        .attr('cx', '50%')
-        .attr('cy', '50%')
-        .attr('r', 20)
-        .style('fill', 'green');
-
-    };
+    
+    const svg = ref(null);
 
     const width = 928;
     const height = 680;
+    const drawMole = () => {
+        
+        console.log("drawing Mole");
+        const mole = d3.select(svg.value); // How to reference a prop in script vue
+        // mole.selectAll('*').remove() // Clear previous renders
+        
+        mole.attr('width', width).attr('height', height)
+
+        console.log(`Selected Mole: ${mole}`);
+        mole.append('circle')
+        .attr('cx', xAxis)
+        .attr('cy', yAxis)
+        .attr('r', 50)
+        .style('fill', 'green');
+
+    };
     onMounted(drawMole);
     watch(() => xAxis, drawMole);
 
-
-    
 </script>
 
 <style>
