060ac42fd7bdd83f4bc845a60092be2a439e01eb
Bea <1123bmac@gmail.com>
2026-04-07 02:28:10 -0700

    add view selection icons and prep for better typing of csv

---
+++ web/app/components/ui/SelectionIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="py-1" :title="displayName">
+    <UIcon
+      :name="name"
+      class="size-7"
+      @click="$emit('click')"
+      :class="{
+        'text-neutral-400': !active,
+        'text-primary-500': active,
+        'hover:text-primary-300': !active
+      }"
+    />
+  </span>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  name: string;
+  displayName?: string;
+  active?: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: "click"): void;
+}>();
+</script>
--- web/app/pages/test.vue
+++ web/app/pages/test.vue
@@ -1,17 +1,45 @@
-<script setup lang="ts">
-import Map from "~/components/map/Map.vue";
-</script>
-
 <template>
   <UDashboardGroup>
     <UDashboardSidebar variant="inset" resizable>
       EVERYTHING IS FINE
     </UDashboardSidebar>
 
-    <div class="p-4 size-full">
-      <div class="overflow-hidden rounded-xl size-full">
+    <div class="p-4 w-full h-10/12">
+      <div class="w-full flex items-end justify-end gap-2">
+        <SelectionIcon
+          name="game-icons:molecule"
+          @click="showMole = !showMole"
+          :active="showMole"
+          displayName="Toggle Mole View"
+        />
+        <SelectionIcon
+          name="game-icons:wireframe-globe"
+          @click="mapMode = 'choropleth'"
+          :active="mapMode === 'choropleth'"
+          display-name="Choropleth View"
+        />
+        <SelectionIcon
+          name="game-icons:river"
+          @click="mapMode = 'lakeVis'"
+          :active="mapMode === 'lakeVis'"
+          display-name="Lake Visualization View"
+        />
+      </div>
+      <div class="overflow-hidden rounded-xl size-full mb-4">
         <Map show-pins />
       </div>
+
+      <div class="p-4 w-full h-2/12 rounded-xl">SLIDER GOES HERE</div>
     </div>
   </UDashboardGroup>
 </template>
+
+<script setup lang="ts">
+import Map from "~/components/map/Map.vue";
+import SelectionIcon from "~/components/ui/SelectionIcon.vue";
+
+type MapModes = "lakeVis" | "choropleth";
+
+const mapMode = ref<MapModes>("lakeVis");
+const showMole = ref(false);
+</script>
